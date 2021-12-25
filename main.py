486c29b1a6e85aff37d4a10b6f14941635430fb7
Papop Lekhapanyaporn <75293379+Retaehc-pop@users.noreply.github.com>
2021-12-25 14:53:01 +0700

    commit

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import Port
 import sys
 import string
 import time
+from Clock import RTC
 from Port import Port
+import threading
 
 
 def clear():
@@ -19,15 +21,48 @@ class controller:
         clear()
         print(f"Starting {project['NAME']} ..................")
         time.sleep(1)
+        self.project = project
+        self.ckp1()
+        print("connection established!")
+        self.display()
+
+    def ckp1(self):
         self.comport = None
         while self.comport is None:
             self.comport = self.choose_port()
 
-        self.PORT = Port(com=self.comport, end=project.end,
-                         file_name=project.file, key=project.PKG)
+        self.PORT = Port(com=self.comport, end=self.project.end,
+                         file_name=self.project.file, key=self.project.PKG)
         temp = input("Ready to connect? y/n : ")
         if temp.lower() == "y":
             self.PORT.connect()
+        else:
+            self.ckp()
+
+    def format_data(self, PKG: dict):
+        prompt = 0
+        data = ""
+        for key, data in PKG:
+            pkg_tmp = f"{key}: {data}"
+            prompt += 1
+            pkg_tmp += "\n" if prompt % 4 == 0 else "\t\t"
+            data += pkg_tmp
+        data += "-------------------------------------------------\n"
+        return data
+
+    def display(self):
+        self.clock = RTC()
+        while True:
+            clear()
+            print(f"""
+    Project:\t\t{self.porject['NAME']}\t\tDate:\t\t{self.clock.RTC.date()}
+    Time:\t\t{self.clock.RTC.time_pc()}\t\tElapsed:\t\t{self.clock.time_elapsed()}
+############################################################################""")
+
+            PKG = self.PORT.reading()
+            toprint = self.format_data(PKG)
+            print(toprint)
+            time.sleep(0.01)
 
     def choose_port(self):
         if len(Port.list_port()) == 0:
